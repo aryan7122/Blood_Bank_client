ecb6c8d870c39aa8ae7f2346be11e0feff02e76e
Aryan <aryankushwahalifenote@gmail.com>
2024-04-06 10:59:54 +0530

    V 1.0.2

--- src/services/API.js
+++ src/services/API.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_BASEURL || 'https://blood-back-server.onrender.com' || 'http://localhost:8081/api/v1/auth'
+  baseURL: process.env.REACT_APP_BASEURL
 });
-  // || 'http://localhost:8081/api/v1/auth' || ''
+// || 'https://blood-back-server.onrender.com' || 'http://localhost:8081/api/v1/auth'
+// || 'http://localhost:8081/api/v1/auth' || ''
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("token")) {
