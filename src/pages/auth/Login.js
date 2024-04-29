a2eadc9415a0ea3437cc96b00ac81e23e7e56869
Aryan <aryankushwahalifenote@gmail.com>
2024-04-29 17:21:26 +0530

    v 1.0.19

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Form from "../../components/shared/Form/Form";
 import { useSelector } from "react-redux";
-// import Spinner from "./../../components/shared/Spinner";
+import Spinner from "./../../components/shared/Spinner";
 
 const Login = () => {
-  const {  error } = useSelector((state) => state.auth);
+  const { loading, error } = useSelector((state) => state.auth);
   return (
     <>
       {error && <span>{alert(error.message)}</span>}
       {console.log('error', error)}
-      {/* {loading ? (
-        <Spinner /> */}
-      {/* ) : ( */}
+      {loading ? (
+        <Spinner /> 
+       ) : (
         <div className="row g-0">
           <div className="col-md-8 form-banner">
             <img src="./assets/images/b2.jpeg" alt="loginImage" />
@@ -24,7 +24,7 @@ const Login = () => {
             />
             </div>
         </div>
-      {/* )} */}
+       )} 
     </>
   );
 };
